3782b64f838167f4a591d42535ca6e2307b44bfe
jihyeChu <09strawberry@naver.com>
2024-12-24 18:33:07 +0900

    test-2

--- src/main/java/com/example/discovery/Practice2.java
+++ src/main/java/com/example/discovery/Practice2.java
@@ -1,8 +1,7 @@
 package com.example.discovery;
 
-public class Practice {
-
+public class Practice2 {
     public static void main(String[] args) {
-        System.out.println("practice");
+        System.out.println("test2");
     }
 }
